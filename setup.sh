5fa93b25412c01e83dddea3cee0759819164e6fe
deathdies <30880522+deathdies@users.noreply.github.com>
2021-11-13 20:16:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,6 +42,7 @@ echo "[ + ] Process Clone And Installation..."
 git clone https://github.com/deathdies/pstd.git "$HOME/.termux" --depth 1
 git clone https://github.com/romkatv/powerlevel10k.git "$HOME/.termux/zsh" --depth 1
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.termux/plugins/zsh-autosuggestions" --depth 1
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting "$HOME/.termux/plugins/fast-syntax-highlighting" --depth 1
 clear
 cat << "EOF"
 ▄───▄────────▄───▄
